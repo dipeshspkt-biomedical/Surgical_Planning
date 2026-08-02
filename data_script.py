588c82834021dca5d92e8c78dbd989923ab6e752
dipeshspkt-biomedical <dipesh@biomedical.edu.np>
2026-08-02 10:57:25 +0545

    Checking for increased number of dataset

--- data_script.py
+++ data_script.py
@@ -71,8 +71,10 @@ class MyData(pl.LightningDataModule):
         self.sample_train_negative,self.sample_val_negative=train_test_split(self.sample_negative,train_size=self.train_size,test_size=self.test_size)
         self.train_files=self.sample_train_positive+self.sample_train_negative
         self.val_files=self.sample_val_positive+self.sample_val_negative
-        self.train_dataset=Dataset(data=self.train_files[0:100],transform=self.train_transform)
-        self.val_dataset=Dataset(data=self.val_files[0:100],transform=self.val_transform)
+        self.subset_train,_=train_test_split(self.train_files,train_size=1000,random_state=42)
+        self.subset_val,_=train_test_split(self.val_files,train_size=1000,random_state=42)
+        self.train_dataset=Dataset(data=self.subset_train,transform=self.train_transform)
+        self.val_dataset=Dataset(data=self.subset_val,transform=self.val_transform)
         
   
     def train_dataloader(self):
